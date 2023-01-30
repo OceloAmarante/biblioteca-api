06fc8f13c3f4f3e6f50c92860bfade23ca27f2ca
Ocelo Amarante <ocelofarias7@gmail.com>
2023-01-30 15:47:02 -0300

    excecao personalizada para objeto nao encontrado

--- src/main/java/com/oceloamarante/biblioteca/service/CategoriaService.java
+++ src/main/java/com/oceloamarante/biblioteca/service/CategoriaService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.oceloamarante.biblioteca.domain.Categoria;
 import com.oceloamarante.biblioteca.repositories.CategoriaRepository;
+import com.oceloamarante.biblioteca.service.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -16,7 +17,8 @@ public class CategoriaService {
 	
 	public Categoria findById(Integer id) {
 		Optional<Categoria> obj = repository.findById(id);
-		return obj.orElse(null);		
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));		
 		
 	}
 }
